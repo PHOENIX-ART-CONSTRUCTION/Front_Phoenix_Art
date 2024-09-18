016958e96a1cd3a227ae21754a39ae702a8cbeb9
Mitsanta_ANDRIAH <hei.mitsanta@gmail.com>
2024-09-18 21:16:43 +0300

    refractor: Updated various components to change font styles, add uppercase text, and modify layout and styling.

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,14 @@ import { Features } from './components/Features';
 import { About } from './components/About';
 import { Services } from './components/Services';
 import Gallery from './components/Gallery';
-import CommentsList from './components/CommentsList'; // Import du composant de la liste des commentaires
-import ContactForm from './components/ContactForm'; // Import du formulaire de contact
+import CommentsList from './components/CommentsList'; 
+import ContactForm from './components/ContactForm';
 import JsonData from './data/data.json';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Suppression de l'import de Link de 'react-scroll'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SmoothScroll from 'smooth-scroll';
 
 const App = () => {
   const [landingPageData, setLandingPageData] = useState({});
-  
-  // État pour les commentaires
   const [comments, setComments] = useState([
     { name: 'John Doe', message: 'Super site web !', avatar: 'https://ui-avatars.com/api/?name=John+Doe' },
     { name: 'Jane Smith', message: 'J\'ai adoré le contenu.', avatar: 'https://ui-avatars.com/api/?name=Jane+Smith' },
@@ -21,12 +20,15 @@ const App = () => {
     { name: 'Mitsanta Andriah', message: 'C\'est vraiment interessant les services qu\'offre cette entreprise Phoenix Art.', avatar: 'https://ui-avatars.com/api/?name=Mitsanta+Andriah' }
   ]);
 
-  // Charger les données du fichier JSON
   useEffect(() => {
     setLandingPageData(JsonData);
+
+    new SmoothScroll('a[href*="#"]', {
+      speed: 1000,
+      speedAsDuration: true,
+    });
   }, []);
 
-  // Fonction pour gérer l'ajout d'un commentaire
   const handleAddComment = (newComment) => {
     setComments((prevComments) => [...prevComments, newComment]);
   };
@@ -45,9 +47,7 @@ const App = () => {
                 <About aboutData={landingPageData.About || {}} whyChooseUsData={landingPageData.WhyChooseUs || {}} />
                 <Services data={landingPageData.Services || []} />
                 <Gallery data={landingPageData.Gallery || {}} />
-                {/* Passer la fonction d'ajout de commentaire à CommentsList */}
                 <CommentsList comments={comments} onAddComment={handleAddComment} />
-                {/* Ajouter le formulaire de contact ici */}
                 <ContactForm />
               </>
             }
