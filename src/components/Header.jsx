669982520d75bda03f7341655b256fac7c9b01b1
Mitsanta_ANDRIAH <hei.mitsanta@gmail.com>
2024-09-20 21:03:35 +0300

    "Refactor Header component: removed comments, updated animation variants, and added color cycle animation to title"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { motion } from 'framer-motion'; 
 
 export const Header = ({ data }) => {
-  // Vérifie si les données ou le titre sont disponibles
+   // Vérifie si les données ou le titre sont disponibles
   if (!data || !data.title) {
-    return null; // Retourne null si les données ou le titre ne sont pas encore chargés
+    return null;// Retourne null si les données ou le titre ne sont pas encore chargés
   }
 
-  // Variants d'animation pour le titre avec un effet de rebond
+ // Variants d'animation pour le titre avec un effet de rebond
   const titleVariants = {
     hidden: { opacity: 0, y: -100 },
     visible: {
@@ -30,35 +30,45 @@ export const Header = ({ data }) => {
       transition: {
         delay: 0.5,
         duration: 1,
-        staggerChildren: 0.3, // Pour faire apparaître les enfants en décalé
+        staggerChildren: 0.3,
+      },
+    },
+  };
+
+  // Animation des couleurs du texte (rouge, bleu ciel et gris)
+  const colorCycle = {
+    initial: { color: '#d42c2b' }, // Rouge initial
+    animate: {
+      color: ['#d42c2b', '#2b7fd4', '#93989d'], // Rouge, bleu ciel, gris
+      transition: {
+        repeat: Infinity,
+        repeatType: 'reverse', // Revenir en arrière après chaque cycle
+        duration: 4, // Durée totale du cycle
       },
     },
   };
 
   return (
     <header className="relative h-screen flex items-center justify-center text-center overflow-hidden">
-      {/* Vidéo de fond */}
       <video autoPlay muted loop className="absolute inset-0 w-full h-full object-cover">
         <source src="./assets/video.mp4" type="video/mp4" />
         Votre navigateur ne supporte pas la balise vidéo.
       </video>
 
-      {/* Couverture semi-transparente */}
       <div className="absolute inset-0 bg-black opacity-50"></div>
 
-      {/* Contenu du header */}
       <div className="relative z-10 p-4 text-white">
-        {/* Titre avec animation */}
+        {/* Titre avec animation de cycle de couleurs */}
         <motion.h1
           className="text-4xl md:text-6xl mb-4 uppercase font-bold"
           initial="hidden"
           animate="visible"
           variants={titleVariants}
+          {...colorCycle} // Ajout de l'animation de cycle de couleurs
         >
           {data.title}
         </motion.h1>
 
-        {/* Sous-titre et CTA avec animation */}
         <motion.div
           initial="hidden"
           animate="visible"
