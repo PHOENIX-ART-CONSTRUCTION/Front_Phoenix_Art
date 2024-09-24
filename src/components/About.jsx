f32d312577735f4ffd448e87de28bb8544819ec4
Mitsanta_ANDRIAH <hei.mitsanta@gmail.com>
2024-09-24 12:18:00 +0300

    Chore: Updated css and including added accessibility attributes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,18 +3,15 @@ import { FaChevronUp } from 'react-icons/fa';
 import { motion, AnimatePresence } from 'framer-motion';
 
 export const About = ({ aboutData, whyChooseUsData }) => {
-  const [isExpanded, setIsExpanded] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false); // État pour gérer l'expansion
 
-  const handleExpandClick = () => {
-    if (!isExpanded) {
-      setIsExpanded(true);
-    }
-  };
+  // Fonction pour gérer l'expansion de la section
+  const handleExpandClick = () => setIsExpanded(true);
+  
+  // Fonction pour gérer la fermeture de la section
+  const handleCollapseClick = () => setIsExpanded(false);
 
-  const handleCollapseClick = () => {
-    setIsExpanded(false);
-  };
-
+  // Vérifie si les données sont disponibles avant le rendu
   if (!aboutData || !whyChooseUsData) {
     return null; // Retourne null si les données ne sont pas encore chargées
   }
@@ -28,9 +25,9 @@ export const About = ({ aboutData, whyChooseUsData }) => {
           <img
             src={aboutData.image}
             alt="À propos de Phoenix Art Construction"
-            className="w-full h-full object-cover opacity-50"
+            className="w-full h-full object-cover opacity-50" // Opacité de l'image
           />
-          <div className="absolute inset-0 bg-black opacity-60"></div>
+          <div className="absolute inset-0 bg-black opacity-60"></div> {/* Superposition noire */}
         </div>
 
         {/* Contenu principal */}
@@ -51,8 +48,8 @@ export const About = ({ aboutData, whyChooseUsData }) => {
       <div className="flex justify-center my-8">
         <motion.button
           onClick={handleExpandClick}
-          whileTap={{ scale: 0.95 }}
-          disabled={isExpanded} // Désactive le bouton lorsqu'il est déjà ouvert
+          whileTap={{ scale: 0.95 }} // Effet de pression
+          disabled={isExpanded} // Désactive le bouton s'il est déjà ouvert
           className={`bg-blue-500 text-white px-6 py-2 rounded-full ${
             isExpanded ? 'opacity-50 cursor-not-allowed' : 'hover:bg-blue-600'
           }`}
@@ -66,34 +63,36 @@ export const About = ({ aboutData, whyChooseUsData }) => {
         {isExpanded && (
           <motion.div
             key="whyChooseUs"
-            initial={{ opacity: 0, height: 0 }}
-            animate={{ opacity: 1, height: 'auto' }}
-            exit={{ opacity: 0, height: 0 }} // Animation de fermeture
-            transition={{ duration: 0.4 }}
+            initial={{ opacity: 0, height: 0 }} // État initial de l'animation
+            animate={{ opacity: 1, height: 'auto' }} // État animé
+            exit={{ opacity: 0, height: 0 }} // État de sortie de l'animation
+            transition={{ duration: 0.4 }} // Durée de l'animation
             className="relative mt-12 mb-12 p-8 bg-[rgba(155,185,230,0.16)] text-black rounded-lg shadow-2xl border mx-12"
           >
             <h3 className="text-xl md:text-5xl font-bold text-black uppercase mb-6 text-center">
               {whyChooseUsData.title}
             </h3>
-            <ol className="list-decimal list-inside space-y-4 text-justify  pl-6">
+            <ol className="list-decimal list-inside space-y-4 text-justify pl-6">
               {whyChooseUsData.details &&
                 whyChooseUsData.details.map((item, index) => (
                   <motion.li
                     key={index}
-                    initial={{ opacity: 0, scale: 0.3, x: -50 }}
-                    animate={{ opacity: 1, scale: 1, x: 0 }}
-                    exit={{ opacity: 0, scale: 0.3, x: -50 }} // Animation de fermeture
-                    transition={{ delay: 0.1 * index }}
-                    className="text-lg "
-                  >{item.title }:
-                    <span className="block text-lg "> {item.description}</span>
+                    initial={{ opacity: 0, scale: 0.3, x: -50 }} // État initial de l'animation de la liste
+                    animate={{ opacity: 1, scale: 1, x: 0 }} // État animé
+                    exit={{ opacity: 0, scale: 0.3, x: -50 }} // État de sortie de l'animation
+                    transition={{ delay: 0.1 * index }} // Délai basé sur l'index
+                    className="text-lg"
+                  >
+                    {item.title}:
+                    <span className="block text-lg"> {item.description}</span>
                   </motion.li>
                 ))}
             </ol>
 
+            {/* Bouton pour fermer la section */}
             <motion.button
               onClick={handleCollapseClick}
-              whileTap={{ scale: 0.95 }}
+              whileTap={{ scale: 0.95 }} // Effet de pression
               className="mt-6 bg-red-500 text-white p-2 flex justify-center items-center rounded-full hover:bg-red-600 transition duration-300"
             >
               <FaChevronUp size={24} />
