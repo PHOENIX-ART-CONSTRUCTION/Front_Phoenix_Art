8e37d2e826e0e85d570c234ce1992b2e42e3c33d
Mitsanta_ANDRIAH <hei.mitsanta@gmail.com>
2024-10-02 22:12:48 +0300

    Refactored updated styles, added error handling, and changed API request

--- src/components/Actu.jsx
+++ src/components/Actu.jsx
@@ -15,6 +15,8 @@ const NewsCarousel = () => {
           throw new Error('Erreur lors de la récupération des actualités');
         }
         const data = await response.json();
+        
+        // Tri par date de création pour afficher les actualités récentes en premier
         const sortedData = data.sort((a, b) => new Date(b.create_at) - new Date(a.create_at));
         setNewsData(sortedData);
         setIsLoading(false);
@@ -56,8 +58,8 @@ const NewsCarousel = () => {
   const { title, description, image, create_at } = newsData[currentNewsIndex];
 
   return (
-    <section className="w-full h-auto bg-gray-50 flex items-center justify-center py-10" >
-      <div className="max-w-5xl w-full h-auto grid grid-cols-1 lg:grid-cols-2 gap-6 bg-[#051D41]  shadow-lg rounded-lg overflow-hidden">
+    <section className="w-full h-auto bg-gray-50 flex items-center justify-center py-10">
+      <div className="max-w-5xl w-full h-auto grid grid-cols-1 lg:grid-cols-2 gap-6 bg-[#051D41] shadow-lg rounded-lg overflow-hidden">
         {/* Texte avec effet slide-in */}
         <div className={`p-6 flex flex-col items-center justify-start text-left space-y-4 transition-transform duration-700 ease-in-out ${fade ? 'translate-x-0 opacity-100' : '-translate-x-10 opacity-0'}`}>
           <h2 className="text-4xl md:text-2xl uppercase text-center font-bold text-[#BE0B0B]">{title}</h2>
@@ -81,7 +83,7 @@ const NewsCarousel = () => {
 const NewsSection = () => {
   return (
     <div className="w-full py-10">
-      <h2 className="text-4xl md:text-4xl font-bold text-center uppercase text-[#051D41] mb-8" >Actualités</h2>
+      <h2 className="text-4xl md:text-4xl font-bold text-center uppercase text-[#051D41] mb-8">Actualités</h2>
       <NewsCarousel />
     </div>
   );
