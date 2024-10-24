46ec30c1c153076cbbb16e7cec57b3941d3d5d2e
Mitsanta_ANDRIAH <hei.mitsanta@gmail.com>
2024-10-24 18:28:29 +0300

    "Updated ContactForm component: changed margin and padding values, added flex classes to input and label containers, and updated image source and styling."

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -33,7 +33,7 @@ const ContactForm = () => {
   };
 
   return (
-    <section id='contact' className="w-full h-screen bg-gray-100 flex items-center justify-center py-10 my-15">
+    <section id='contact' className="w-full h-screen bg-gray-100 flex items-center justify-center py-10 my-25">
       <div className="max-w-7xl w-full h-auto grid grid-cols-1 lg:grid-cols-2 gap-6 bg-[#051D41] text-[#be0b0b] shadow-lg rounded-lg overflow-hidden">
         {/* Formulaire de contact */}
         <motion.div
@@ -45,34 +45,34 @@ const ContactForm = () => {
           <h2 className="text-2xl md:text-3xl font-bold uppercase text-center">Contactez-nous</h2>
           {error && <p className="text-red-500">{error}</p>}
           <form onSubmit={handleSubmit} className="w-full space-y-6">
-            <div>
-              <label htmlFor="name" className="block text-gray-400 font-bold mb-2">Nom:</label>
+            <div className="flex flex-col">
+              <label htmlFor="name" className="block text-gray-400 font-bold">Nom:</label>
               <input
                 type="text"
                 id="name"
                 name="name"
                 value={formData.name}
                 onChange={handleInputChange}
-                className="border p-2 mb-4 w-full text-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#be0b0b]"
+                className="border p-2  w-full text-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#be0b0b]"
                 placeholder="Entrez votre nom"
                 required
               />
             </div>
-            <div>
-              <label htmlFor="email" className="block text-gray-400 font-bold mb-2">Email:</label>
+            <div className="flex flex-col">
+              <label htmlFor="email" className="block text-gray-400 font-bold">Email:</label>
               <input
                 type="email"
                 id="email"
                 name="email"
                 value={formData.email}
                 onChange={handleInputChange}
-                className="border p-2 mb-4 w-full rounded-lg focus:outline-none focus:ring-2 focus:ring-[#be0b0b]"
+                className="border p-2 w-full rounded-lg focus:outline-none focus:ring-2 focus:ring-[#be0b0b]"
                 placeholder="Entrez votre email"
                 required
               />
             </div>
-            <div>
-              <label htmlFor="message" className="block text-gray-400 font-bold mb-2">Message:</label>
+            <div className="flex flex-col">
+              <label htmlFor="message" className="block text-gray-400 font-bold ">Message:</label>
               <textarea
                 id="message"
                 name="message"
@@ -80,7 +80,7 @@ const ContactForm = () => {
                 onChange={handleInputChange}
                 className="border p-2 w-full rounded-lg resize-none focus:outline-none focus:ring-2 focus:ring-[#be0b0b]"
                 placeholder="Votre message"
-                rows="5"
+                rows="2"
                 required
               ></textarea>
             </div>
@@ -97,10 +97,10 @@ const ContactForm = () => {
           initial={{ opacity: 0, x: 100 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.5 }}
-          className="relative w-full h-full flex items-center justify-center"
+          className="relative w-full h-[421px] flex mt-2 items-center justify-center"
         >
           <img
-            src="/assets/Contact.jpg"
+            src="/assets/Contact.jpeg"
             alt="Contact"
             className="w-full h-full object-cover transition-opacity duration-1000 ease-in-out"
           />
